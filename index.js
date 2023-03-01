5ad93e24daded1cda9eda858d97481792e099779
ltram16 <ltram16@gmail.com>
2023-02-28 22:34:32 -0800

    included packages and created an array of questions in index.js

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'List the table of contents.',
+        name: 'tableofcontents',
+    },
+    {
+        type: 'input',
+        message: 'State the installation instructions.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'State the usage information.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What license do you use for this project?',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'State the contribution guidelines.',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'State the test instructions.',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What is the link to your GitHub profile?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
